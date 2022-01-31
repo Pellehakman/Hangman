308c36c5e0f545944a612570715e5816f3a73e77
Pellehakman <perhakman@outlook.com>
2022-01-31 17:29:10 +0100

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -5,16 +5,17 @@ const getWord = document.querySelector('.get-word');
 
 // Lägger in lite grund ord vi kan utöka vid önskan
 
-const randomWords =  ['PÅSK', 'HARE', 'GODIS', 'HÄXA', 'ÄGG'];
+const randomWords =  ['påsk', 'hare', 'godis', 'häxa', 'ägg'];
 
 
  //Lite grund variabler, bra att ha functioner i spelet efter flödesschemat 
 
 let randomWord = '';
-let numberOfIncorrectGuesses = 0;
+let correctGuessed = 0;
+let guessesLeft = 5;
 let numberOfCorrectGuesses = 0;
-let guessKeys = [];  // Gissat bokstav (rätt)
-let  guessedKeyIncorrect = []; // Gissat bokstav (fel)
+let pressedKeys = [];  // Gissat på (generelt)
+let guessedKeyIncorrect = []; // Gissat bokstav (fel)
 
 
 // Funktion för att generera ett randomord från påsk array ovan
@@ -35,7 +36,7 @@ getWord.addEventListener('click', () => {
 
     for(let i = 0; i < word.length; i++){
         letterBox.innerHTML += 
-        `<div class="flex input__letter__true"><span class="hide" data-letter=" ${word[i]} "> ${word[i]}</span></div>`;
+        `<div class="flex input__letter__true"><span class="hide" data-letter="${word[i]}"> ${word[i]}</span></div>`;
     }
 })
 
@@ -43,21 +44,66 @@ function testLetter(str) {
     return /^[a-öA-Ö]+$/.test(str);
 }
 
+function guessedletters(str) {
+    let test = false;
+    for(let i = 0; i < pressedKeys.length +1; i++){
+        if(pressedKeys[i] == str){
+            return test;
+        } else {
+            pressedKeys.push(str)
+            test = true;
+            console.log(test)
+            return test;
+        }
+    }
+}
+
+function paintImg() {
+    const man = document.querySelector('.figure__box');
+    guessesLeft = guessesLeft - 1;
+
+    if(guessesLeft == 4){
+        man.classList.add('scaffold')
+    } else if (guessesLeft == 3){
+        man.classList.add('head')
+    } else if (guessesLeft == 2){
+        man.classList.add('body')
+    } else if (guessesLeft == 1){
+        man.classList.add('arms')
+    } else if (guessesLeft == 0){
+        man.classList.add('legs')
+        
+    setTimeout( () => {
+        document.querySelector('.end__word').innerHTML = randomWord;
+        document.querySelector('.end__box').classList.add('show')
+    }, 1000)
+    }
+}
 
 document.addEventListener('keydown', (evt) => {
     const guessedLetter = evt.key;
-    let check = testLetter (guessedLetter);
+    let checkAlpha = testLetter(guessedLetter);
+    let checkPressed = guessedletters(guessedLetter)
     let divLetter = '';
 
     let letters = document.querySelectorAll('.input__letter__true');
-    console.log(letters)
-    if(check == true) {
+    console.log(checkAlpha, checkPressed)
+    if(checkAlpha == true && checkPressed == true) {
         for(let letter of letters) {
-            divLetter = letter.getAttribute('data-letter')
-            console.log(divLetter)
-            if(letter == guessedLetter){
-                letter.getElementByTagName('span').classlist.toggle('hide');
+            divLetter = letter.firstChild.getAttribute('data-letter');
+            if(divLetter == guessedLetter){
+                letter.firstChild.classList.remove('hide');
+                correctGuessed++;
+                numberOfCorrectGuesses++;
+                if(numberOfCorrectGuesses == randomWord.length){
+                    document.querySelector('.win__word').innerHTML = randomWord;
+                    document.querySelector('.win__box').classList.add('show')
+                }
             }
         }
+        if(correctGuessed < 1) {
+            paintImg()
+        }
+        correctGuessed = 0;
     }
 })
