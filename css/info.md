3c057d081188dcdd70d0ec9fe2633c688cf44d67
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-02-01 18:28:45 +0100

    update 02/01
    fixed clock, estetics and a new correction system

--- css/info.md
+++ css/info.md
@@ -1,9 +1,36 @@
 KVAR ATT GÖRA
-1. ord ska finnas utan att man ska trycka starta spel.
+<!-- 1. ord ska finnas utan att man ska trycka starta spel. -->
 2. Ta bort "Skriv en bokstav för att starta spelet!" när man skrivit en bokstav.
-3. countdown number of try (så den räknar ner försök) 
+<!-- 3. countdown number of try (så den räknar ner försök)  -->
 4. koppla starta om knappen (vid avslutat spel)  
-5. bara bokstäver ska användas
-6. fyll på felaktiga bokstäver
-7. countdown timer
-8. skapa init?
+<!-- 5. bara bokstäver ska användas -->
+<!-- 6. fyll på felaktiga bokstäver  -->
+<!-- 7. countdown timer -->
+<!-- '9. kolla så att inmatat värde bara är en karaktär' -->
+<!-- 10. byt ut så att man gemför med en array som är kopplat till randomWord -->
+
+KRITERIER!
+
+Kunna applicera datalogiskt tänkande och gå från idé till kod
+Visa förståelse och kunna använda sig av programmeringens byggstenar (variabler, villkor, funktioner, loopar etc)
+Krav på funktionalitet
+Du ska bygga det klassiska spelet hänga gubbe.
+Det ska vara gjort med HTML/CSS/Javascript
+Användaren ska kunna mata in med tangentbordet bokstäver
+Användaren ska kunna se vilka bokstäver den gissar rätt på och var i ordet de hamnar
+Vid varje fel ska en del av gubben visas
+Ifall användaren gissar på rätt ord så ska en ”Du vann”-skärm visas med en fråga om man vill spela igen,
+Ifall användaren inte hinner gissa rätt ska en ”Du förlorade”-skärm visas med det rätta ordet och en fråga om man vill spela igen.
+Du ska enbart kunna gissa på en bokstav i taget.
+Ordet får inte vara hårdkodat i Javascript-filen när den ska jämföras
+Betygskriterier
+Godkänt
+
+Alla krav på funktionalitet är uppfyllda och följs.
+Att SVG:en som bifogas i HTML:en används
+Spelet fungerar med inga fel i konsolen i developer tools.
+Vettiga namn på variabler och funktioner på engelska.
+Väl Godkänt
+
+Allt i godkänt
+Att du kan köra hänga gubbe på tid. En nedräkning visas på 1 minut där användaren automatiskt förlorar om tiden går ut. I detta fall kör man alltid på tid.
--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,10 @@
 // Lägger till HTML element i JS
 
 const getWord = document.querySelector('.get-word');
+const falseBox = document.querySelector('.letter__box__false');
+const restartButton = document.querySelectorAll('.restart__btn');
+const man = document.querySelector('.figure__box');
+const triesLeft = document.querySelector('.guesses__quant')
 
 // Lägger in lite grund ord vi kan utöka vid önskan
 
@@ -10,11 +14,14 @@ const randomWords =  ['påsk', 'hare', 'godis', 'häxa', 'ägg'];
 
 //Lite grund variabler, bra att ha functioner i spelet efter flödesschemat 
 
+let counter = 10;
+let startStopCounter = false;
 let randomWord = '';
 let correctGuessed = 0;
 let guessesLeft = 5;
 let numberOfCorrectGuesses = 0;
 let pressedKeys = [];  // Gissat på (generelt)
+let correctGuesses = []; // Gissat bokstav (rätt)
 let guessedKeyIncorrect = []; // Gissat bokstav (fel)
 
 
@@ -23,46 +30,62 @@ let guessedKeyIncorrect = []; // Gissat bokstav (fel)
 function generateRandomWord(){
     let random = Math.floor(Math.random() * randomWords.length);
     randomWord = randomWords[random];
-    return randomWord
-;
-
-}
+    correctGuesses = [];
+    pressedKeys = [];
+    showWord(randomWord)
+    console.log(randomWord)
+;}
 
 getWord.addEventListener('click', () => {
+    generateRandomWord()
+})
+
+function showWord(word){ //Adds HTML code of the random Word
     const letterBox = document.querySelector('.letter__box__true')
     letterBox.innerHTML = '';
-
-    let word = generateRandomWord();
+    falseBox.innerHTML = '';
 
     for(let i = 0; i < word.length; i++){
         letterBox.innerHTML += 
         `<div class="flex input__letter__true"><span class="hide" data-letter="${word[i]}"> ${word[i]}</span></div>`;
     }
-})
-
-function testLetter(str) {
-    return /^[a-öA-Ö]+$/.test(str);
 }
 
-function guessedletters(str) {
-    let test = false;
-    for(let i = 0; i < pressedKeys.length +1; i++){
-        if(pressedKeys[i] == str){
-            test = false
-            return test;
-        } else {
-            pressedKeys.push(str)
-            test = true;
-            console.log(test)
-            return test;
-        }
+function testLetter(str) { // Testing letters
+    const test = /[a-öA-Ö]/.test(str);
+    if(str.length > 1) {
+        alert('Please enter an alphabetic character')
+    } else if (test == false){
+        alert('Please enter an alphabetic character')
+    } else {
+        return /[a-öA-Ö]/.test(str);
     }
 }
 
-function paintImg() {
-    const man = document.querySelector('.figure__box');
-    guessesLeft = guessesLeft - 1;
+function guessedletters(str) { // Testing letters
+    let test = true;
+    for(let i = 0; i < pressedKeys.length +1; i++){
+        if(pressedKeys[i] == str){
+            alert('You can´t press the same key twice')
+            test = false
+            return test;
+        }
+    }
+    if(test == true) {
+        pressedKeys.push(str);
+        return test
+    }
+}
 
+function addWrongletters(str) { // Adds wrong letter guesses to HTML
+    falseBox.innerHTML +=
+    `<div class="flex input__letter__false"><span id="">${str}</span></div>`;
+}
+
+
+function paintImg() { //Paints the hangman picture
+    guessesLeft = guessesLeft - 1;
+    triesLeft.innerHTML = guessesLeft;
     if(guessesLeft == 4){
         man.classList.add('scaffold')
     } else if (guessesLeft == 3){
@@ -74,37 +97,116 @@ function paintImg() {
     } else if (guessesLeft == 0){
         man.classList.add('legs')
         
-    setTimeout( () => {
+    setTimeout( () => { // Display that u lost
         document.querySelector('.end__word').innerHTML = randomWord;
         document.querySelector('.end__box').classList.add('show')
     }, 1500)
     }
 }
 
-document.addEventListener('keydown', (evt) => {
-    const guessedLetter = evt.key;
-    let checkAlpha = testLetter(guessedLetter);
-    let checkPressed = guessedletters(guessedLetter)
-    let divLetter = '';
-
-    let letters = document.querySelectorAll('.input__letter__true');
-    console.log(checkAlpha, checkPressed)
-    if(checkAlpha == true && checkPressed == true) {
-        for(let letter of letters) {
-            divLetter = letter.firstChild.getAttribute('data-letter');
-            if(divLetter == guessedLetter){
-                letter.firstChild.classList.remove('hide');
-                correctGuessed++;
-                numberOfCorrectGuesses++;
-                if(numberOfCorrectGuesses == randomWord.length){
+function testWon(str) { //Adds score and determine if u won
+    let notFound = true;
+    for(let i = 0; i < correctGuesses.length + 1; i++) {
+        if(str == correctGuesses[i]) {
+            notFound = false
+        }
+    }
+    if(notFound == true) {
+        for(let j = 0; j < randomWord.length; j++) {
+            if(str == randomWord[j]) {
+                correctGuesses.push(str)
+                console.log(correctGuesses)
+                if(correctGuesses.length == randomWord.length) {
                     document.querySelector('.win__word').innerHTML = randomWord;
                     document.querySelector('.win__box').classList.add('show')
                 }
             }
         }
+    }
+}
+
+
+document.addEventListener('keydown', (evt) => { //Register keystroke
+    const guessedLetter = evt.key;
+    let checkAlpha = testLetter(guessedLetter);
+    let checkPressed = guessedletters(guessedLetter);
+    let dataLetter = '';
+
+    if (startStopCounter == false) {
+        startTimer(); 
+    }
+
+    startStopCounter = true;
+
+    let letters = document.querySelectorAll('.input__letter__true');
+    console.log(checkAlpha, checkPressed)
+    if(checkAlpha == true && checkPressed == true) { // checks if pressed key is neither a non alphabetic letter or has been used before
+        for(let letter of letters) {
+            dataLetter = letter.firstChild.getAttribute('data-letter');
+            if(dataLetter == guessedLetter){
+                letter.firstChild.classList.remove('hide');
+                correctGuessed++;
+                testWon(guessedLetter)
+            }
+        }
         if(correctGuessed < 1) {
             paintImg()
+            addWrongletters(guessedLetter)
         }
-    correctGuessed = 0;
+        guessedKeyIncorrect.push(guessedLetter)
+        correctGuessed = 0;
     }
 })
+console.log(randomWord)
+console.log()
+generateRandomWord(randomWord)
+
+
+// countdown timer
+
+function startTimer() {
+    const timerIntervalId = setInterval(() => {
+        counter--;
+        if(counter > 0){
+    
+            id = document.getElementById('count');
+            id.innerHTML = counter;
+        } else if (counter <= 0) { // Display that u lost
+            document.querySelector('.end__word').innerHTML = randomWord;
+            document.querySelector('.end__box').classList.add('show')
+        }
+    }, 1000) 
+    getWord.addEventListener('click', () =>{
+        clearInterval(timerIntervalId);
+        restart();
+    })
+}
+
+    for(let i = 0; i < restartButton.length; i++){
+    restartButton[i].addEventListener('click', () => {
+        totalRestart()
+    })
+}
+
+
+function restart() {
+    startStopCounter = false;
+    counter = 10;
+    id.innerHTML = counter;
+    
+    man.classList.remove('scaffold') 
+    man.classList.remove('head')
+    man.classList.remove('body')
+    man.classList.remove('arms')
+    man.classList.remove('legs')
+
+    guessesLeft = 5
+    triesLeft.innerHTML = guessesLeft;
+}
+
+function totalRestart() {
+    restart()
+    document.querySelector('.end__box').classList.remove('show')
+    document.querySelector('.win__box').classList.remove('show')
+    generateRandomWord()
+}
